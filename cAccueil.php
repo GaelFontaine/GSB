8e1895d64d50e999695c3197353e12b993fd81f8
Anaïs DELATTRE <anais.delattre1@gmail.com>
2015-10-07 15:48:03 +0200

    Test

--- cAccueil.php
+++ cAccueil.php
@@ -18,7 +18,7 @@
   <!-- Division principale -->
   <div id="contenu">
       <h2>Bienvenue sur l'intranet GSB</h2>
-      <h3>Test</h3>
+      <h3>Test number 1</h3>
   </div>
 <?php        
   require($repInclude . "_pied.inc.html");
--- include/class.pdogsb.inc.php
+++ include/class.pdogsb.inc.php
@@ -17,9 +17,9 @@
 
 class PdoGsb{   		
       	private static $serveur='mysql:host=localhost';
-      	private static $bdd='dbname=gsb';   		
-      	private static $user='userGsb' ;    		
-      	private static $mdp='secret' ;	
+      	private static $bdd='dbname=gsb_frais';   		
+      	private static $user='root' ;    		
+      	private static $mdp='' ;	
 		private static $monPdo;
 		private static $monPdoGsb=null;
 /**
